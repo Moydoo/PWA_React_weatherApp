083d1814658468d25de889ccf5b7ececc4d732a4
Michal <m.stefanski98@gmail.com>
2022-06-29 17:28:26 +0200

    PWA implementation | Cache service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html', 'offline.html'];
+
+const self = this;
+
+//Install Service Worker
+//Open the cache and save them
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+//Listen for requests
+//We match all the request our site is saving and then fetch them again
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                //if it can't fetch the data, it means we do not have net connection
+                //so it have to return offline.html
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+
+});
+
+//Activate the Service Worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
